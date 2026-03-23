fb5366e7a6940e55b00c9c79ed26125a76264faa
Gaura <awadhvastram@gmail.com>
2026-03-23 20:28:36 +0530

    fix port final

--- backend-server/server.js
+++ backend-server/server.js
@@ -196,6 +196,6 @@ app.use((err, req, res, next) => {
 // ==========================
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, () => {
     console.log("🚀 Server running on port " + PORT);
 });
